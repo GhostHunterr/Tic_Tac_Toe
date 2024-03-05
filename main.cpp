f1493795972da79e58da34b19557c4241ccbe1dd
Farhan <farhansarang7@gmail.com>
2024-03-05 02:14:05 -0500

    Program is Complete.

--- main.cpp
+++ main.cpp
@@ -3,16 +3,16 @@
 
 using namespace std;
 
-//Initializes the board.
+// Initializes the board.
 void initializeBoard(vector<vector<char>> &board);
 
-//Prints the content of the board.
+// Prints the content of the board.
 void printBoard(const vector<vector<char>> &board);
 
-//This function checks if a player has won or not.
+// This function checks if a player has won or not.
 bool results(vector<vector<char>> &board);
 
-//This Function takes input, validates and stores it in our 2-D vector Board.
+// This Function takes input, validates and stores it in our 2-D vector Board.
 void gameInput(const int num, vector<vector<char>> &board);
 
 int main()
@@ -20,12 +20,11 @@ int main()
     vector<vector<char>> board;
     initializeBoard(board);
     printBoard(board);
-    for (int i = 0; i < 9 && results(board); i++)
+    for (int i = 0; i < 9 && !results(board); i++)
     {
         gameInput((i % 2) + 1, board);
         printBoard(board);
     }
-
     return 0;
 }
 
@@ -45,20 +44,50 @@ void printBoard(const vector<vector<char>> &board)
 
 bool results(vector<vector<char>> &board)
 {
+    char player = 'X';
+    for (int i = 0; i <= 2; i++)
+    {
+        if ((board[i][0] == player && board[i][1] == player && board[i][2] == player) || (board[0][i] == player && board[1][i] == player && board[2][i] == player))
+        {
+            cout << "Player 1, has Won!" << endl;
+            return true;
+        }
+    }
+    if ((board[0][0] == player && board[1][1] == player && board[2][2] == player) || (board[0][2] == player && board[1][1] == player && board[2][0] == player))
+    {
+        cout << "Player 1, has Won!" << endl;
+        return true;
+    }
 
+    player = 'O';
+    for (int i = 0; i <= 2; i++)
+    {
+        if ((board[i][0] == player && board[i][1] == player && board[i][2] == player) || (board[0][i] == player && board[1][i] == player && board[2][i] == player))
+        {
+            cout << "Player 2, has Won!" << endl;
+            return true;
+        }
+    }
+    if ((board[0][0] == player && board[1][1] == player && board[2][2] == player) || (board[0][2] == player && board[1][1] == player && board[2][0] == player))
+    {
+        cout << "Player 2, has Won!" << endl;
+        return true;
+    }
+
+    return false;
 }
 
 void gameInput(const int num, vector<vector<char>> &board)
 {
     bool flag = true;
     string input;
-    cout << "input in (x,y) format." << endl;
+    cout << "input in (row,column) format." << endl;
     cout << "Input the position for player " << num << ": ";
     do
     {
         flag = true;
         cin >> input;
-        if (!(input[0] <= '2' && input[0] >= '0') || !(input[2] <= '2' && input[2] >= '0') || input[1] != ',')
+        if (input.length() != 3 || !(input[0] <= '2' && input[0] >= '0') || !(input[2] <= '2' && input[2] >= '0') || input[1] != ',')
         {
             flag = false;
             cout << "!!!Input is in wrong Format.!!!" << endl
